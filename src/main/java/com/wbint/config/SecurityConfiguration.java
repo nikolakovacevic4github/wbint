d699eea4b3bd5ede6891e2f26c85dc20445f0984
Nemanja Antic <n.antic@levi9.com>
2022-01-13 14:13:39 +0100

    Conf v3

--- src/main/java/com/wbint/config/SecurityConfiguration.java
+++ src/main/java/com/wbint/config/SecurityConfiguration.java
@@ -64,36 +64,48 @@ public class SecurityConfiguration extends AADResourceServerWebSecurityConfigure
                 .build();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web
-            .ignoring()
-            .antMatchers(HttpMethod.OPTIONS, "/**")
-            .antMatchers("/app/**/*.{js,html}")
-            .antMatchers("/i18n/**")
-            .antMatchers("/content/**")
-            .antMatchers("/swagger-ui/**")
-            .antMatchers("/test/**");
-    }
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web
+//            .ignoring()
+//            .antMatchers(HttpMethod.OPTIONS, "/**")
+//            .antMatchers("/app/**/*.{js,html}")
+//            .antMatchers("/i18n/**")
+//            .antMatchers("/content/**")
+//            .antMatchers("/swagger-ui/**")
+//            .antMatchers("/test/**");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//        http.authorizeRequests()
+//                .antMatchers( "/oauth2/**", "/login/**" ).permitAll().antMatchers("/api/authenticate").permitAll()
+//            .antMatchers("/api/register").permitAll()
+//            .antMatchers("/api/activate").permitAll()
+//            .antMatchers("/api/account/reset-password/init").permitAll()
+//            .antMatchers("/api/account/reset-password/finish").permitAll()
+//            .antMatchers("/api/admin/**").authenticated()
+//            .antMatchers("/api/wb/**").permitAll()
+//            .antMatchers("/api/**").authenticated()
+//            .antMatchers("/management/health").permitAll()
+//            .antMatchers("/management/health/**").permitAll()
+//            .antMatchers("/management/info").permitAll()
+//            .antMatchers("/management/prometheus").permitAll()
+//            .antMatchers("/management/**").authenticated()
+//                .and().oauth2Login();
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-                .antMatchers( "/oauth2/**", "/login/**" ).permitAll().antMatchers("/api/authenticate").permitAll()
-            .antMatchers("/api/register").permitAll()
-            .antMatchers("/api/activate").permitAll()
-            .antMatchers("/api/account/reset-password/init").permitAll()
-            .antMatchers("/api/account/reset-password/finish").permitAll()
-            .antMatchers("/api/admin/**").authenticated()
-            .antMatchers("/api/wb/**").permitAll()
-            .antMatchers("/api/**").authenticated()
-            .antMatchers("/management/health").permitAll()
-            .antMatchers("/management/health/**").permitAll()
-            .antMatchers("/management/info").permitAll()
-            .antMatchers("/management/prometheus").permitAll()
-            .antMatchers("/management/**").authenticated()
-                .and().oauth2Login();
+                .antMatchers( "/oauth2/**", "/login/**" ).permitAll()
+                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login()
+                .and()
+                .oauth2Client();;
     }
 
     private JWTConfigurer securityConfigurerAdapter() {
